3e675f99ec6337e0e2979a989ab22fd32f0f7dae
Maalik Brown <mbrown29@zagmail.gonzaga.edu>
2022-11-08 18:15:18 -0800

    LightsOutGame implemented

--- app/src/main/java/com/example/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/example/pizzaparty/MainActivity.kt
@@ -1,46 +1,124 @@
 package com.example.pizzaparty
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.GridLayout
+import android.widget.Toast
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import kotlin.math.ceil
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 
-private const val KEY_TOTAL_PIZZAS = "totalPizzas"
+
 
 const val TAG = "MainActivity"
+const val GAME_STATE = "gameState"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var numAttendEditText: EditText
     private lateinit var numPizzasTextView: TextView
     private lateinit var howHungryRadioGroup: RadioGroup
-    private var totalPizzas = 0
+    private lateinit var game: LightsOutGame
+    private lateinit var lightGridLayout: GridLayout
+    private var lightOnColor = 0
+    private var lightOffColor = 0
+    private var lightOnColorId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate was called")
+        lightOnColorId = R.color.yellow
+        lightGridLayout = findViewById(R.id.light_grid)
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
         howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
-        if (savedInstanceState != null) {
-            totalPizzas = savedInstanceState.getInt(KEY_TOTAL_PIZZAS)
-            displayTotal()
+
+        for (gridButton in lightGridLayout.children) {
+            gridButton.setOnClickListener(this::onLightButtonClick)
+        }
+
+        lightOnColor = ContextCompat.getColor(this, R.color.yellow)
+        lightOffColor = ContextCompat.getColor(this, R.color.black)
+
+        game = LightsOutGame()
+        if (savedInstanceState == null) {
+            startGame()
+        }
+        else {
+            game.state = savedInstanceState.getString(GAME_STATE)!!
+            setButtonColors()
+        }
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            outState.putString(GAME_STATE, game.state)
         }
     }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(KEY_TOTAL_PIZZAS, totalPizzas)
+    private fun startGame() {
+        game.newGame()
+        setButtonColors()
     }
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        totalPizzas = savedInstanceState.getInt(KEY_TOTAL_PIZZAS)
-        displayTotal()
+    fun onChangeColorClick(view: View) {
+        val intent = Intent(this, ColorActivity::class.java)
+        colorResultLauncher.launch(intent)
+        intent.putExtra(EXTRA_COLOR, lightOnColorId)
     }
+    val colorResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val colorId = result.data!!.getIntExtra(EXTRA_COLOR, R.color.yellow)
+            lightOnColor = ContextCompat.getColor(this, colorId)
+            lightOnColorId = result.data!!.getIntExtra(EXTRA_COLOR, R.color.yellow)
+            lightOnColor = ContextCompat.getColor(this, lightOnColorId)
+            setButtonColors()
+        }
+    }
+    private fun onLightButtonClick(view: View) {
+
+        // Find the button's row and col
+        val buttonIndex = lightGridLayout.indexOfChild(view)
+        val row = buttonIndex / GRID_SIZE
+        val col = buttonIndex % GRID_SIZE
+
+        game.selectLight(row, col)
+        setButtonColors()
+
+        // Congratulate the user if the game is over
+        if (game.isGameOver) {
+            Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun setButtonColors() {
+
+        // Set all buttons' background color
+        for (buttonIndex in 0 until lightGridLayout.childCount) {
+            val gridButton = lightGridLayout.getChildAt(buttonIndex)
+
+            // Find the button's row and col
+            val row = buttonIndex / GRID_SIZE
+            val col = buttonIndex % GRID_SIZE
+
+            if (game.isLightOn(row, col)) {
+                gridButton.setBackgroundColor(lightOnColor)
+            } else {
+                gridButton.setBackgroundColor(lightOffColor)
+            }
+        }
+    }
+
+    fun onNewGameClick(view: View) {
+        startGame()
+    }
+
+
 
     fun calculateClick(view: View) {
 
@@ -51,25 +129,21 @@ class MainActivity : AppCompatActivity() {
         val numAttend = numAttendStr.toIntOrNull() ?: 0
 
         // Get hunger level selection
-        val hungerLevel = when (howHungryRadioGroup.getCheckedRadioButtonId()) {
-            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
-            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
-            else -> PizzaCalculator.HungerLevel.RAVENOUS
+        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> 2
+            R.id.medium_radio_button -> 3
+            else -> 4
         }
 
         // Get the number of pizzas needed
-        val calc = PizzaCalculator(numAttend, hungerLevel)
-        totalPizzas = calc.totalPizzas
-        displayTotal()
+        val totalPizzas = ceil(numAttend * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
+        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+
 
         // Place totalPizzas into the string resource and display
-        val totalText = getString(R.string.total_pizzas, totalPizzas)
-        numPizzasTextView.setText(totalText)
+
 
     }
 
-    private fun displayTotal() {
-        val totalText = getString(R.string.total_pizzas, totalPizzas)
-        numPizzasTextView.text = totalText
-    }
+
     }
